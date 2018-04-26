648320dff1a6d977651db5baaa862d244628bea1
Rasmus Jönsson <rasmus@rasmusj.se>
2018-04-26 11:15:18 +0200

    Created backend implementation for basic services management

---
+++ src/backend/kubefront/services/routes.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	corev1 "github.com/ericchiang/k8s/apis/core/v1"
+	"github.com/gin-gonic/gin"
+	"github.com/rctl/kubefront/src/backend/kubefront/core"
+)
+
+//Service is an instance of the authentication API handler
+type Service struct {
+	ctx *core.Context
+}
+
+//Routes setup routes for the Authentication API
+func Routes(r *gin.RouterGroup, ctx *core.Context) {
+	s := &Service{
+		ctx: ctx,
+	}
+	//Setup routes with need for authentication
+	r.Use(core.AuthMiddleware(ctx))
+	{
+		r.GET("/", s.list)
+		r.GET("/:namespace/", s.listNamespace)
+		r.DELETE("/:namespace/:id", s.delete)
+	}
+	go func(s *Service) {
+		//Watch for node changes
+		var service corev1.Service
+		watcher, err := s.ctx.Client.Watch(s.ctx, s.ctx.Config.Namespace, &service)
+		defer watcher.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+		for {
+			n := new(corev1.Service)
+			watcher.Next(n)
+			s.ctx.NotifySubscribers("SERVICES", &core.Message{
+				Action: "SERVICE_CHANGED",
+				Entity: *n.Metadata.Name,
+				Data:   n,
+			})
+		}
+	}(s)
+}
